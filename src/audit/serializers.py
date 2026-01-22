cd4d28c45d113037f852edc31154f51f1d395830
Donchess1 <stepabod@yahoo.com>
2026-01-22 21:32:45 +0100

    slim audit log serializer

--- src/audit/serializers.py
+++ src/audit/serializers.py
@@ -4,6 +4,13 @@ from audit.models import AuditLog
 
 
 class AuditLogSerializer(serializers.ModelSerializer):
+    ip_address = serializers.SerializerMethodField()
     class Meta:
         model = AuditLog
-        fields = "__all__"
+        fields = ["user_name", "user_role", "action", "audit_type", "audit_module",
+                  "ip_address", "created_at", ]
+        
+    def get_ip_address(self, obj):
+        if not obj.request_meta:
+            return None
+        return obj.request_meta.get('ip_address')
